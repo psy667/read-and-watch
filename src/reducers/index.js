ada73c6677fceb45d1874e51c88f59a87bf1609a
Arseniy Oguzov <arseniyoguzov@gmail.com>
2019-08-23 14:07:12 +0300

    init

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,33 +1,57 @@
-import { combineReducers } from 'redux';
-import { handleActions } from 'redux-actions';
-import * as actions from '../actions';
-import axios from 'axios';
+import { combineReducers } from "redux";
+import * as actions from "../actions";
+import axios from "axios";
 
-const test = handleActions({
-  [actions.increment](state, { payload : { task }}) {
-    const { value } = state;
-    return {
-      ...state,
-      value: value + 1
-    }
-  },
-  [actions.testRequest](state){
-    return state
-  },
-  [actions.testFailure](state){
-    return state
-  },
-  [actions.testSuccess](state, {payload: { data }}) {
-    console.log(data);
-    const { items } = state;
-    return {
-      ...state,
-      items: data
-    }
+// const test = handleActions(
+//   {
+//     [actions.increment](
+//       state,
+//       {
+//         payload: { task }
+//       }
+//     ) {
+//       const { value } = state;
+//       return {
+//         ...state,
+//         value: value + 1
+//       };
+//     },
+//     [actions.testRequest](state) {
+//       return state;
+//     },
+//     [actions.testFailure](state) {
+//       return state;
+//     },
+//     [actions.testSuccess](
+//       state,
+//       {
+//         payload: { data }
+//       }
+//     ) {
+//       console.log(data);
+//       const { items } = state;
+//       return {
+//         ...state,
+//         items: data
+//       };
+//     }
+//   },
+//   { value: 0, items: [] }
+// );
+
+const initialState = {};
+
+const test = (state = initialState, action) => {
+  switch (action.type) {
+    case "ACTION":
+      break;
+
+    default:
+      return {};
+      break;
   }
-}, {value: 0, items: []});
-
+};
 
 export default combineReducers({
   test
-})
+});
