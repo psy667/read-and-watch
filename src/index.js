8e2af1ebd588503a79e14d1f4ee7e01b20044ed8
psy667 <arseniyoguzov@gmail.com>
2019-09-03 23:37:29 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -15,10 +15,11 @@ import * as serviceWorker from "./serviceWorker";
 
 const store = createStore(
     reducers,
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    ),
+    // compose(
+    // applyMiddleware(thunk),
+    // eslint-disable-next-line no-underscore-dangle
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    // ),
 );
 
 ReactDOM.render(
